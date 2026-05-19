fd5166a2cb85cae5c208ac830f074e27c40f1c96
Diogo Viegas <76049967+Diogo-Viegas@users.noreply.github.com>
2026-05-19 07:36:34 +0000

    add function

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -5,19 +5,24 @@ PhoneBook::PhoneBook()
     this->index = 0;
 }
 
-
-PhoneBook::addContact()
+void PhoneBook::addContact()
 {
-    
-    std::string name;
+    std::string first,last,nick,phone,secret;
+    int current_index = this->index % 8;
     std::cout << "Enter First Name: " << std::endl;
-    std::getline(std::cin,name);
+    std::getline(std::cin,first);
+    std::cout << "Enter Last Name: " << std::endl;
+    std::getline(std::cin,last);
+    std::cout << "Enter Nickname" << std::endl;
+    std::getline(std::cin,nick);
+    std::cout << "Enter Darkest Secret" << std::endl;
+    
+    this->contacts[current_index].saveFirstName(first);
+    this->contacts[current_index].saveLastName(last);
+    this->contacts[current_index].saveNickname(nick);
+    this->contacts[current_index].savePhone(phone);
+    this->contacts[current_index].saveSecret(secret);
 
+    std::cout << "Contact Sucessfuly Added" << std::endl;
 }
-    int  PhoneBook::getIndex()
-    {
-         return(this->index); 
-    }
-{
-    return(this.index)
-}
+
